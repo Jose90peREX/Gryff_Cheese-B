1f59fc500779dc05bf57b541432f5ea58fc5a0a2
33zzzz <156396100+33zzzz@users.noreply.github.com>
2025-03-01 23:54:20 -0800

    Renamed a file and added temporary benchmarking to main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <cstdio>
+#include <vector>
+#include <numeric>
+#include <random>
+#include <chrono>
+#include <"quickSort.h">
+#include <"SelectionSort.h">
+using namespace std;
+using namespace chrono;
+
+//Benchmarking:
+template <typename T>
+void RunTimedTest(const string& test_name, T&& funct) {
+    auto start = high_resolution_clock::now();
+    function();
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end-start);
+    cout << "[TIME] " << test_name << ": " << duration.count() << "\n"
+}
 
 int main() {
-    printf("asd\n");
+    vector<int> testVec(10000);
+    iota(testVec.begin(), testVec.end(), 1);
+
+    std::random_device rd;
+    std::mt19937 r(randumb());
+
+    RunTimedTest("Selection Sort", [&]() {
+        vector<int> copyTest = testVec;
+        shuffle(copyTest.begin(), copyTest.end(), r);
+        selectionSort(copyTest, testVec.size()-1);
+    });
+
     return 0;
 }
