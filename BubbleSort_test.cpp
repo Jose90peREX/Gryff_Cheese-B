e7c220671c3d66dbbaf9dbe1c2baa231385c36bc
barrera_0943104 <0943104@my.scccd.edu>
2025-03-02 22:23:30 -0800

    Test for bubble sort

---
+++ BubbleSort_test.cpp
@@ -0,0 +1,15 @@
+   #include "gtest/include/gtest/gtest.h"
+   #include "BubbleSort.h"
+   #include <vector>
+   using namespace std;
+     
+     TEST(UnsortedVector, BubbleSortedTest)
+
+       vector<int> v = {2, 1, 5, 3, 4};
+        BubbleSort(v);   
+     
+       vector<int> result = {1, 2, 3, 4, 5};
+       BubbleSort(result);
+              
+         EXPECT_EQ(v, result);
+   }
