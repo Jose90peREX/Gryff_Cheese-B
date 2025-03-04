10180745e481a8babf42cb0ecc945cf96805f5a7
33zzzz <zmsilverio04@gmail.com>
2025-03-03 18:52:35 -0800

    Quick Sort tests

--- quickSort_test.cpp
+++ quickSort_test.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 using namespace std;
 
+//Unsorted
 TEST(UnsortedQuick, QuickSortTest) {
     vector<int> vec(1000000); //Vector sized 1,000,000
     iota(vec.begin(), vec.end(), 1);
@@ -20,5 +21,67 @@ TEST(UnsortedQuick, QuickSortTest) {
     ASSERT_EQ(vec, sorted_vec);
 }
 
+//Reversed
+TEST(ReversedQuick, QuickSortTest) {
+   vector<int> vec(1000000);
+   iota(vec.begin(), vec.end(), 1);
+   vector<int> sorted_vec = vec;
 
+   reverse(vec.begin(), vec.end());
+   quickSort(vec.data(), 0, vec.size()-1, 0);
+   ASSERT_EQ(vec, sorted_vec); 
+}
+
+//Sorted
+TEST(SortedQuick, QuickSortTest) {
+    vector<int> vec(1000000);
+    iota(vec.begin(), vec.end(), 1);
+    vector<int> sorted_vec = vec;
+    quickSort(vec.data(), 0, vec.size()-1, 0);
+    ASSERT_EQ(vec, sorted_vec);
+}
+
+//Small Vector
+TEST(SmallQuick, QuickSortTest) {
+    vector<int> vec(10);
+    iota(vec.begin(), vec.end(), 1);
+    vector<int> sorted_vec = vec;
+
+    std::random_device rd;
+    unsigned seed = rd();
+    std::mt19937 r(seed);
+
+    shuffle(vec.begin(), vec.end(), r);
+    quickSort(vec.data(), 0, vec.size()-1, 0);
+    ASSERT_EQ(vec, sorted_vec);
+}
+
+//Negative Vector
+TEST(NegativeQuick, QuickSortTest) {
+    vector<int> vec(1000000);
+    for (int i = 0; i < vec.size(); i++) { vec[i] = i - 500000; }
+    vector<int> sorted_vec = vec;
+    sort(sorted_vec.begin(), sorted_vec.end());
+
+    std::random_device rd;
+    unsigned seed = rd();
+    std::mt19937 r(seed);
+    shuffle(vec.begin(), vec.end(), r);
+    quickSort(vec.data(), 0, vec.size()-1, 0);
+    ASSERT_EQ(vec, sorted_vec);
+}
+
+//Partially Sorted
+TEST(PartialQuick, QuickSortTest) {
+    vector<int> vec(1000000);
+    iota(vec.begin(), vec.end(), 1);
+    vector<int> sorted_vec = vec;
+    
+    std::random_device rd;
+    unsigned seed = rd();
+    std::mt19937 r(seed);
+    shuffle(vec.begin(), vec.begin() + vec.size()/2, r); //Shuffles the first half
+    quickSort(vec.data(), 0, vec.size()-1, 0);
+    ASSERT_EQ(vec, sorted_vec);
+}
 
